92275e9c13a41db241896a25ff63d8aaa10b99de
wagix01 <adewuyi.ismaila@yahoo.com>
2022-10-17 10:10:53 -0700

    lowlevel 02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+
+}
