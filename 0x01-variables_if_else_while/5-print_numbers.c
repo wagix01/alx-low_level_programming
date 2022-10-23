bf1340e09c95f53414f7a4a3faeebcd1dbf7b81c
wagix01 <adewuyi.ismaila@yahoo.com>
2022-10-23 06:48:43 -0700

    lowlevel 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program that
+ * print single base number base 10'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int x;
+
+	for (x = 0; x <= 9; x++)
+
+
+		printf("%u", x);
+
+
+	printf("\n");
+
+
+	return (0);
+}
